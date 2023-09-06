5bfc3e8ab414131c9c2693e435f40e9def0a28d0
JALINDAR01 <134129652+JALINDAR01@users.noreply.github.com>
2023-09-06 10:00:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
 //your code here
 
+let bandNames=['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal']
+
+function sortBandNames(names) {
+	const articles=['a','an','the'];
+	return names.sort((a,b)=>{
+		const aName=a.toLowerCase().replace(/^(the|a|an) /,'');
+		const bName=b.toLowerCase().replace(/^(the|a|an) /,'');
+		return aName.toLocalCompare(bName);
+	});
+	
+}
+
+const sortedbandnames = sortBandNames(bandNames);
+
+const ulElement = document.getElementById('band');
+
+sortedbandnames.forEach(name=>{
+	const liElement=document.createElement('li');
+	liElement.textContent=name;
+	ulElement.appendchild(liElement);
+}) ;
+	
